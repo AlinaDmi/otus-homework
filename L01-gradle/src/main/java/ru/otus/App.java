ee43eab87610b4f222e16c8d8a751f49b975c15b
dmitrieva <dmitrieva@advantum.pro>
2023-11-08 14:27:33 +0300

    first homework

---
+++ L01-gradle/src/main/java/ru/otus/App.java
@@ -0,0 +1,21 @@
+package ru.otus;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("java:S106")
+public class App {
+    public static void main(String... args) {
+
+        List<Integer> result = new ArrayList<>();
+        int min = 0;
+        int max = 10;
+        for (int i = min; i < max; i++) {
+            result.add(i);
+        }
+
+        System.out.println(Lists.reverse(result));
+    }
+}

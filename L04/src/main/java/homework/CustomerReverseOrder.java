a1eb4432485fd4eb518d468ff23bcf8655d58d9e
dmitrieva <dmitrieva@advantum.pro>
2023-11-27 13:03:28 +0300

    second homework

--- L04/src/main/java/homework/CustomerReverseOrder.java
+++ L04/src/main/java/homework/CustomerReverseOrder.java
@@ -1,13 +1,13 @@
 package homework;
 
 
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 
 public class CustomerReverseOrder {
 
     // todo: 2. надо реализовать методы этого класса
     // надо подобрать подходящую структуру данных, тогда решение будет в "две строчки"
-    LinkedList<Customer> customers = new LinkedList<>();
+    private final ArrayDeque<Customer> customers = new ArrayDeque<>();
 
     public void add(Customer customer) {
         customers.addLast(customer);
--- L04/src/main/java/homework/CustomerService.java
+++ L04/src/main/java/homework/CustomerService.java
@@ -7,7 +7,7 @@ import java.util.TreeMap;
 
 public class CustomerService {
 
-    NavigableMap<Customer, String> scores = new TreeMap<>((o1, o2) -> (int) (o1.getScores() - o2.getScores()));
+    private final NavigableMap<Customer, String> scores = new TreeMap<>((o1, o2) -> (int) (o1.getScores() - o2.getScores()));
 
     // todo: 3. надо реализовать методы этого класса
     // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
